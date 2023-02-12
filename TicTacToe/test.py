8367b08ce4fbdf9a4df319139c9c4460425799c9
konstantin <87504288+pulseneon@users.noreply.github.com>
2023-02-12 20:45:59 +0300

    Delete test.py

--- TicTacToe/test.py
+++
@@ -1,9 +0,0 @@
-from language.langs import Language
-
-langs = Language()
-def get_string(string: str) -> str:
-    return langs.get_string(string)
-
-print(get_string("curr_chat_lansg"))
-print(get_string("curr_chat_lang"))
-#print(langs.get_languages())
