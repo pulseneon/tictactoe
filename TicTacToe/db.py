0b4e746c958b073befadb18be8c4992c23487c0c
pulseneon <thebooom@yandex.ru>
2023-02-13 15:04:11 +0300

    add user model

--- TicTacToe/db.py
+++ TicTacToe/db.py
@@ -1,5 +1,25 @@
-# db connect from postgresql
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+class Base(DeclarativeBase): pass
 
 class Database:
     def __init__(self) -> None:
+        load_dotenv()
+        db_user = os.getenv('DB_USER')
+        db_pass = os.getenv('DB_PASSWORD')
+        db_host = os.getenv('DB_HOST')
+        db_name = os.getenv('DB_NAME')
+        
+        postgresql_database = f'postgresql://{db_user}:{db_pass}@{db_host}/{db_name}'
+
+        engine = create_engine(postgresql_database)
+
+        Base.metadata.create_all(bind=engine) # create db ?
+        Session = sessionmaker(autoflush=False, bind=engine)
+
+    def register_user(self, user) -> True or False:
         pass
--- TicTacToe/models/user.py
+++ TicTacToe/models/user.py
@@ -1,1 +1,13 @@
-# user.py model
+from sqlalchemy import BigInteger, Column, Integer, String
+from TicTacToe.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(BigInteger)
+    user_name = Column(String)
+    games_count = Column(Integer)
+    lose_count = Column(Integer)
+    wins_count = Column(Integer)
+    lang = Column(String)
